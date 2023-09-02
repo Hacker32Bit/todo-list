c5d79e82d9048a43a22e84aebf17b4546cc56d0b
Gektor Gevorgyan <gektor.gevorgyan@gmail.com>
2023-09-02 19:50:47 +0400

    Added functionality for Important button and Done

--- src/components/TodoList/TodoListItem/TodoListItem.js
+++ src/components/TodoList/TodoListItem/TodoListItem.js
@@ -4,22 +4,48 @@ import "./todo-list-item.css";
 import { FaTrash, FaInfo, FaCheck } from "react-icons/fa6";
 
 class TodoListItem extends Component {
+  state = {
+    isDone: false,
+    isImportant: this.props.important
+  };
+
+  onDone = () => {
+    this.setState({
+      isDone: !this.state.isDone
+    });
+
+    console.log("Done - ", this.props.text);
+  };
+
+  onImportant = () => {
+    this.setState({
+      isImportant: !this.state.isImportant
+    })
+
+    console.log("Important - ", this.state.isImportant)
+  }
+
   render() {
-    const { text, important } = this.props;
-    const style = {
-      color: important ? "#d72020" : "black",
-      fontWeight: important ? "bold" : "normal",
+    const { text } = this.props;
+    const { isDone, isImportant } = this.state;
+
+    const textStyle = {
+      textDecoration: isDone ? "line-through" : "none",
+      color: isDone ? "#aaa" : isImportant ? "red" : "black",
+      fontWeight: isDone ? "normal" : isImportant ? "bold" : "normal",
     };
 
     return (
-      <li className="list-item" style={style}>
-        <span className="item-text">{text}</span>
+      <li className="list-item">
+        <span className="item-text" style={textStyle} onClick={ this.onDone }>
+          {text}
+        </span>
 
         <span className="item-btns">
-          <button className="item-btn-done">
+          <button className="item-btn-done" onClick={ this.onDone }>
             <FaCheck />
           </button>
-          <button className="item-btn-important">
+          <button className="item-btn-important" onClick={ this.onImportant }>
             <FaInfo />
           </button>
           <button className="item-btn-remove">
